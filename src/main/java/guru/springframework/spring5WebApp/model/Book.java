9acff130699fdc04ed288903dcd343fc9ebcc95e
HungTran <tmhung.nt@gmail.com>
2019-05-22 20:24:11 +0700

    first MVC sample: - adding Book Controller - using thymeleaf to show dynamic html (similar with ejs file from express library)

--- src/main/java/guru/springframework/spring5WebApp/model/Book.java
+++ src/main/java/guru/springframework/spring5WebApp/model/Book.java
@@ -37,6 +37,14 @@ public class Book {
         this.authors = authors;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
     }
